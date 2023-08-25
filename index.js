8f7227792f70b725cf0172a1314217a01dca4dce
EmoCookie <129533088+EmoCookie@users.noreply.github.com>
2023-08-25 18:12:53 +0530

    Add files via upload
    A simple JavaScript project that asks a few questions related to Geography...!
    
    Visual Studio Code | JavaScript | Node

---
+++ index.js
@@ -0,0 +1,12 @@
+// Performing a task
+function greet(name, lastName) {
+    console.log('Breathe In ' + name + ' ' + lastName + '!');
+}
+
+// Calculate a value
+function square(number) {
+    return number * number;
+}
+
+greet('John', 'Cena');
+console.log(square(2));
